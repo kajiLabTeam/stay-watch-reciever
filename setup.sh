1916b793c7c14060aa99a2336a1f48b324affe1e
happy663 <53854298+happy663@users.noreply.github.com>
2022-04-02 01:56:11 +0900

    sh change

--- setup.sh
+++ setup.sh
@@ -1,4 +1,21 @@
 sudo pip3 install schedule
+sudo nano /etc/systemd/system/autostart.servicen
 
+[Unit]
+Description=do something
+ConditionPathExists=/home/pi/stay-watch-reciever
+
+[Service]
+ExecStart=/home/pi/stay-watch-reciever/newstart.sh
+Restart=no
+Type=simple
+
+[Install]
+WantedBy=multi-user.target
+
+sudo chown root:root /etc/systemd/system/autostart.service
+sudo chmod 644 /etc/systemd/system/autostart.service
+sudo systemctl enable /etc/systemd/system/autostart.service
+sudo reboot
 
 
