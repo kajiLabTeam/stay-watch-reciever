2e16aa0eb12ac5cca1a6c04e67076951b331af0e
happy663 <53854298+happy663@users.noreply.github.com>
2022-05-13 18:32:11 +0900

    fileName change

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ sudo pip3 install schedule
 sudo pip3 install bluepy
 
 sudo nano /etc/systemd/system/autostart.servicen
-cp /home/pi/stay-watch-reciever/autostart.service  /etc/systemd/system/
+sudo cp /home/pi/stay-watch-reciever/autostart.service  /etc/systemd/system/
 
 sudo chown root:root /etc/systemd/system/autostart.service
 sudo chmod 644 /etc/systemd/system/autostart.service
