7accdb345eadba35df946ec51459fa80b2ef180b
happy663 <53854298+happy663@users.noreply.github.com>
2022-05-13 14:56:09 +0900

    設定2回目

--- autorun.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-while true
-do
-	sudo python3 /home/pi/stay-watch/receiver.py
-done
--- setup.sh
+++ setup.sh
@@ -1,17 +1,6 @@
 sudo pip3 install schedule
 sudo nano /etc/systemd/system/autostart.servicen
-
-[Unit]
-Description=do something
-ConditionPathExists=/home/pi/stay-watch-reciever
-
-[Service]
-ExecStart=/home/pi/stay-watch-reciever/newstart.sh
-Restart=no
-Type=simple
-
-[Install]
-WantedBy=multi-user.target
+cp /home/pi/stay-watch-reciever/autostart.service  /etc/systemd/system/
 
 sudo chown root:root /etc/systemd/system/autostart.service
 sudo chmod 644 /etc/systemd/system/autostart.service
