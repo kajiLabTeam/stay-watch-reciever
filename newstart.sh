aea0fc3686a25844a1038ca44bfc4b0503c248d7
happy663 <53854298+happy663@users.noreply.github.com>
2022-05-15 13:38:04 +0900

    scan chnge

--- newstart.sh
+++ newstart.sh
@@ -2,6 +2,8 @@
 while true
 do
 	sleep 10
-	sudo python3 /home/pi/stay-watch-reciever/uuid_scanner.py -t 60
+	sudo python3 /home/pi/stay-watch-reciever/uuid_scanner.py -t 120
+	sudo hciconfig hci0 down
+	sudo hciconfig hci0 up
 done
 
--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 sudo pip3 install schedule
 sudo pip3 install bluepy
 sudo pip3 install requests
-
 sudo cp /home/pi/stay-watch-reciever/autostart.service  /etc/systemd/system/
 
+sudo chmod +x /home/pi/stay-watch-reciever/newstart.sh
 sudo chown root:root /etc/systemd/system/autostart.service
 sudo chmod 644 /etc/systemd/system/autostart.service
 sudo systemctl enable /etc/systemd/system/autostart.service
