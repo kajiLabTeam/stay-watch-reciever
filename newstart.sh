5098612ce2b92de758162630c8bda8834dc93185
happy663 <53854298+happy663@users.noreply.github.com>
2022-05-15 23:47:21 +0900

    scan time change

--- newstart.sh
+++ newstart.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 while true
 do
-	sleep 10
-	sudo python3 /home/pi/stay-watch-reciever/uuid_scanner.py -t 120
+	sleep 30
+	sudo python3 /home/pi/stay-watch-reciever/uuid_scanner.py -t 180
 	sudo hciconfig hci0 down
 	sudo hciconfig hci0 up
 done
