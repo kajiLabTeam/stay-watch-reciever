5985fd65861cc216ff68be77a47d4889d8a9f202
happy663 <53854298+happy663@users.noreply.github.com>
2022-05-16 14:19:17 +0900

    test

--- newstart.sh
+++ newstart.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 while true
 do
-	sleep 10
-	sudo python3 /home/pi/stay-watch-reciever/uuid_scanner.py -t 60
+	sleep 60
+	sudo python3 /home/pi/stay-watch-reciever/uuid_scanner.py -t 240
 	sudo hciconfig hci0 down
 	sudo hciconfig hci0 up
 done
